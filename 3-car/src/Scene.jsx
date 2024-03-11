98c6728a909dbbd93e4d24b472afc027eec9ba52
gloomystore <pikchu3333@gmail.com>
2024-03-11 12:50:04 +0900

    fix: 중력 조절

--- 3-car/src/Scene.jsx
+++ 3-car/src/Scene.jsx
@@ -2,12 +2,20 @@ import { Canvas } from "@react-three/fiber";
 import { Box } from "./Box";
 import { Ground } from "./Ground";
 import { Debug, Physics } from '@react-three/cannon'
+import { useControls } from 'leva'
 
 function Scene() {
+  const bgValue = useControls({bgColor: '#fff'})
+  const gravity = useControls('Gravity', {
+    x: { value: 0, min: -10, max: 10, step: 0.1}, // min max step 넣으면 조절 바가 생김
+    y: { value: -9.81, min: -10, max: 10, step: 0.1},
+    z: { value: 0, min: -10, max: 10, step: 0.1},
+  })
   return (
     <>
       <Canvas camera={{ position: [0, 2, 4] }}>
-        <Physics gravity={[0, -9.81, 0]}>{/* m/s 단위인듯? */}
+        <color attach={'background'} args={[bgValue.bgColor]} />
+        <Physics gravity={[gravity.x, gravity.y, gravity.z]}>{/* m/s 단위인듯? */}
           <Debug>
             <ambientLight/>
             <directionalLight position={[0, 5, 5]} />
